f3d7f0e91182348ec799d711fe2f3803e0db4955
stoneman06 <99403099+stoneman06@users.noreply.github.com>
2023-05-05 16:29:28 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+final databaseReference = FirebaseDatabase.instance.reference();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Image _image;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  void getData() {
+    databaseReference.child('esp32-cam/photo').onValue.listen((event) {
+      if (event.snapshot.value is String) {
+        final image = Image.memory(base64Decode(event.snapshot.value as String));
+        setState(() {
+          _image = image;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: _image ?? Text('Loading...'),
+      ),
+    );
+  }
+}
